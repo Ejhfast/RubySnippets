7c50e58d95809bf2df76bf891d0cd792aaf84838
Ethan Fast <ethan@dnab404638.stanford.edu>
2013-09-04 17:29:41 -0700

    first

---
+++ patterns.rb
@@ -0,0 +1,1188 @@
+if var0[var1]
+  var2[var1] = var0[var1]
+end
+
+var0.to_s.split("str0").last
+
+((var0[:sym0]) || (var0["str0"]))
+
+var0.cookies.each
+
+var0 == var1[0]
+
+params[:sym0].to_i
+
+var0.x - var1.x
+
+var0["str0"].collect
+
+config.action_controller.perform_caching=(false)
+
+self.name == var0.name
+
+set_backtrace(var0.backtrace)
+
+"str0" + var0.length.to_s
+
+var0.concat(var1.reverse)
+
+var0[var1.to_sym] = var2
+
+self[position_column] = bottom_position_in_list.to_i + 0
+
+var0.content_type=(var1["str0"])
+
+var0.split.each
+
+var0.class == self.class
+
+acts_as_list_class.update_all("#{position_column}str0#{position_column}str1", "#{scope_condition}")
+
+def method
+  raise(NotImplementedError.new("str0"))
+end
+
+var0["str0"][var1] = var2
+
+var0.collect do |var1, **vr0|
+  var1
+end.uniq
+
+def method(var0)
+  var1 = var0[:sym0]
+  var2 = var0[:sym1]
+end
+
+var0 << var1.name
+
+var0.class.to_s
+
+if options[:sym0]
+  "str0"
+else
+  "str1"
+end
+
+var0.constants.each
+
+User.find(params[:sym0])
+
+var0[0] != "str0"
+
+if var0.empty?
+  nil
+else
+  var0.join("str0")
+end
+
+var0 = var0.uniq.sort
+
+var0 = options[:sym0]
+
+0 - var0.size
+
+var0 = File.join(File.dirname("str0"), "str1")
+
+unless var0 =~ /str0/
+  raise(EOFError, "str1")
+end
+
+var0.is_a?(self.class)
+
+config.filter_parameters += [:sym0]
+
+var0.clear
+var1.clear
+
+!var0.include?(var1)
+
+config.i18n.fallbacks=(true)
+
+record do |var0|
+  var0.migration_template("str0", "str1")
+end
+
+if Hash === var0.last
+  var0.pop
+else
+  {}
+end
+
+var0 = ("str0" + var1.to_s) + "str1"
+
+var0.puts(var1.inspect)
+
+var0.to_i - 0
+
+(var0 - var1) > 0
+
+Dir[File.join(var0, "str0", "str1")]
+
+var0.write(var1.join("str0"))
+
+var0.keys.map
+
+var0.keys.collect do |var1|
+  var1.to_s
+end
+
+path.sub(/str0/, "str1")
+
+var0[var1, var2 - var1]
+
+var0[:sym0] != "str0"
+
+$3[0, 1].downcase
+
+var0[0].to_f
+
+
+  var0, var1 = var2.coerce(self)
+  return(var0 <=> var1)
+rescue NoMethodError
+
+Dir[File.dirname("str0") + "str1"].each do |var0|
+  require(var0)
+end
+
+var0.sub(/str0/n, "str1").downcase
+
+if ((var0) && (!var0.closed?))
+  var0.close
+end
+
+((var0) && (var0[0] == "str0"))
+
+Format::ABBR_MONTHS[$2.downcase]
+
+"str0#{options[:sym0]}str1"
+
+var0.values.first
+
+var0.first["str0"]
+
+var0.each do |var1, var2|
+  var3[var1.to_s] = var2
+end
+
+var0 = var1.split("str0")[0]
+
+var0 > var1.size
+
+var0.to_s.downcase
+
+var0[var1.object_id]
+
+return(var0[var1 - 0])
+
+var0.on("str0", "str1", "str2") do |var1|
+  var2[:sym0] = var1
+end
+
+if var0.respond_to?(:sym0)
+  var0.close
+end
+
+var0.bytes.to_a
+
+var0 = var1[var2[0]]
+
+Thread.current[:sym0] = var0
+
+var0 - var1[var2]
+
+var0.type.to_s
+
+type != :sym0
+
+var0.abs / 0
+
+var0[-var1..0]
+
+var0.value == var1
+
+File.dirname("str0") + "str1"
+
+Rails.application.config
+
+(0 * 1) + 2
+
+((var0 + "str0") + var1) + var2
+
+var0[:sym0].each
+
+File.exist?(File.join(var0, "str0"))
+
+"str0" => var0.size.to_s
+
+var0.to_i >= 0
+
+var0 = var0.to_s.strip
+
+var0.close
+var1.close
+
+var0 = var1[var2 + 0]
+
+children.each
+
+var0.name == var1.to_s
+
+var0.xml do
+  render({:sym0 => var1})
+end
+
+var0[0].each
+
+if ((var0.nil?) || (var0.empty?))
+  raise(EOFError, "str0")
+end
+
+var0 << var1.slice!(0, var2)
+
+var0.size > 0
+
+var0.type == "str0"
+
+var0.version == var1
+
+"str0#{params[:sym0]}str1"
+
+var0 = var1 - var2.size
+
+Array(var0).each
+
+var0.body=(var1.body)
+
+var0.each do |var1|
+  var1.join
+end
+
+var0[var1 + 0..1]
+
+var0.map do |var1|
+  var1.to_sym
+end
+
+return(var0[var1][var2])
+
+var0[0].class
+
+File.expand_path("str0", File.dirname("str1"))
+
+((var0 == 0) && (var1 == 0))
+
+Time.now.to_f
+
+caller.join("str0")
+
+var0 + caller
+
+var0[:sym0]["str0"]
+
+var0.data["str0"]
+
+config.action_view.debug_rjs=(true)
+
+config.assets.digest=(true)
+
+var0.reverse.join("str0")
+
+Post.find(params[:sym0])
+
+var0.message.split("str0")
+
+var0[0, var1.length]
+
+(var0 & 0) != 1
+
+var0[:sym0] = [var0[:sym0]]
+
+source_root(File.expand_path("str0", "str1"))
+
+if var0.is_a?(Hash)
+  var1.merge!(var0)
+end
+
+flash.now
+
+var0.size <= var1
+
+var0.send(var1.to_sym)
+
+[var0[0..0], format_datetime(var1), var2, var0, var3, msg2str(var4)]
+
+var0.to_s.gsub(/str0/, "str1").gsub(/str2/, "str3").gsub(/str4/, "str5")
+
+"str0" + var0.inspect
+
+if defined?(RAILS_GEM_VERSION)
+  RAILS_GEM_VERSION
+else
+  if ENV.include?("str0")
+    ENV["str0"]
+  else
+    parse_gem_version(read_environment_rb)
+  end
+end
+
+var0.size != var1.size
+
+var0.flatten.join("str0")
+
+params["str0"]
+
+var0.length - 0
+
+var0.delete(Thread.current)
+
+require(File.expand_path(var0))
+
+File.stat(var0).mode
+
+var0[var1].length
+
+def method
+  Thread.current[:sym0]
+end
+
+var0[:sym0].last
+
+URI.parse(var0["str0"])
+
+(var0 & 0) >> 1
+
+if var0.user
+  var1.basic_auth(var0.user, var0.password)
+end
+
+!File.exists?(var0)
+
+config.action_controller.consider_all_requests_local=(true)
+
+Thread.current[:sym0] = []
+
+var0 = var1[0].to_s
+
+var0[var1] = var2.clone
+
+var0.to_s.split(/str0/)
+
+"str0#{Time.now - var0}str1"
+
+var0.last << var1
+
+var0 = (var1 - var2).abs
+
+Time.parse(var0["str0"])
+
+var0.to_s.camelize
+
+var0 = Post.new(var1.path)
+
+if var0.is_a?(Hash)
+  var1.update(var0)
+end
+
+var0.text == var1
+
+!var0.respond_to?(:sym0)
+
+each do |var0|
+  var1 << yield(var0)
+end
+
+var0.to_a.collect
+
+var0 << var1.to_s
+
+((options[:sym0]) || (options[:sym1]))
+
+((var0[:sym0]) && (!var0[:sym1]))
+
+def method
+  update_attribute(position_column, 0)
+end
+
+((var0) && (var1.size >= var0))
+
+h(var0.name)
+
+var0.sort_by do |var1, var2|
+  -var2
+end
+
+((var0.is_a?(Array)) && (var0.size == 0))
+
+config.action_dispatch
+
+var0 = var1.to_i * 0
+
+send(position_column).to_i
+
+((var0 == true) || (var0 == false))
+
+var0.gsub(/str0/, "str1").gsub(/str2/, "str1")
+
+raise(StandardError.new("str0"))
+
+!var0.is_a?(String)
+
+var0.to_s.size
+
+:sym0 => var0.class.name
+
+var0.times.map
+
+RbConfig::CONFIG["str0"] + RbConfig::CONFIG["str1"]
+
+var0 = ((var1[0]) || (1)).to_f
+
+var0 + var1[:sym0]
+
+var0.body.to_s
+
+var0[0].is_a?(String)
+
+var0.length == 0
+
+var0[var1.type]
+
+("str0" + var0.join("str1")) + "str2"
+
+Process.pid.to_s
+
+var0.to_s.humanize
+
+self.class.load_rubygems
+load_rails_gem
+require("str0")
+
+var0.split(/str0/).map do |var1|
+  var1.capitalize
+end
+
+name + "str0"
+
+File.basename(var0.path)
+
+require(File.expand_path("str0", "str1"))
+
+object.class
+
+if ((var0) && (var0.size > 0))
+  var1 = var0
+  var0 = nil
+end
+
+var0.map do |var1, var2|
+  "str0#{var1}str1#{var2}"
+end.join("str2")
+
+var0 = var1["str0"].to_s
+
+headers[var0]
+
+((var0[var1]) || (var2[var1]))
+
+raise(Exception.new("str0"))
+
+((var0 == :sym0) || (var0 == :sym1))
+
+request.fullpath
+
+!var0.is_a?(Array)
+
+(var0 - var1) + var2
+
+var0["str0"].blank?
+
+Rational(utc_offset, 0)
+
+var0.first.to_s
+
+var0.to_s.gsub(/str0/) do
+  "str1#{$1.upcase}"
+end.gsub(/str2/) do
+  $1.upcase
+end
+
+((ENV["str0"]) && (ENV["str1"]))
+
+config.whiny_nils=(true)
+
+((var0.nil?) && (var1.nil?))
+
+var0.find do |var1|
+  var1.name == var2
+end
+
+var0[var1] + var2
+
+if var0.kind_of?(Symbol)
+  var0 = var0.to_s
+end
+
+var0[0].empty?
+
+var0[var0.size - 0, 0]
+
+type.to_s
+
+var0 = params[:sym0].to_i
+
+if ((defined?(MOD_RUBY)) && (!ENV.key?("str0")))
+  Apache.request.setup_cgi_env
+end
+
+((var0 + var1) + var2) + var3
+
+def method
+  unless lower_item
+    return
+  end
+  acts_as_list_class.transaction do
+    lower_item.decrement_position
+    increment_position
+  end
+end
+
+logger.info("str0")
+
+["#{var0}str0#{var1}"].pack("str1").delete("str2")
+
+if var0 <= 0
+  raise(ArgumentError, "str0")
+end
+
+config.action_controller.consider_all_requests_local=(false)
+
+var0 << ("str0" + var1) + "str1"
+
+var0 = var1[:sym0].to_s
+
+var0["str0"] == false
+
+((((var0 == "str0") || (var0 == "str1"))) || (var0 == "str2"))
+
+"str0#{params[:sym0]}"
+
+cache[var0]
+
+require(File.expand_path(File.join(File.dirname("str0"), "str1", "str2", "str3")))
+
+var0 == var1.size
+
+var0.kind_of?(self.class)
+
+var0 < var1 + var2
+
+((!var0) || (var0.empty?))
+
+var0.dup.delete_if
+
+var0.default.nil?
+
+var0 == var1.length - 0
+
+var0.split("str0").last
+
+!respond_to?(var0)
+
+var0[var0.keys.first]
+
+var0.size - var1
+
+("str0" + "str1") + "str2"
+
+name.pluralize
+
+"str0" + var0.join("str1")
+
+self.class.primary_key
+
+var0[0] != :sym0
+
+("str0" * var0) + var1
+
+rand(0).to_s(1)
+
+var0[var1.downcase]
+
+unless String === var0
+  var0 = var0.to_s
+end
+
+var0[var1] = var2.first
+
+var0.size != var1
+
+if ((var0.nil?) || (var0.empty?))
+  return
+end
+
+config.action_mailer.delivery_method=(:sym0)
+
+options[var0]
+
+var0.new(var1.request_uri)
+
+var0[var1][0]
+
+if block_given?
+  var0 << var1
+end
+
+if ((var0[0, 1] == "str0") || (var0.size > 2))
+  var1, var0, var2 = var0, var2, var1
+end
+
+var0[var1].is_a?(Array)
+
+var0.delete("str0").to_s
+
+var0.length >= var1
+
+var0.delete(var1.object_id)
+
+("str0#{var0}str1" + "str2") + "str3"
+
+Bundler.require(*Rails.groups({:sym0 => ["str0", "str1"]}))
+
+decrement_positions_on_lower_items
+assume_bottom_position
+
+response["str0"]
+
+var0.exec(var1, var2, edit_path(var0.path))
+
+var0[:sym0] = var1.shift
+
+require("str0")
+require("str1")
+require("str2")
+require("str3")
+
+var0.class.to_s.underscore
+
+autoload(:sym0, "str0")
+autoload(:sym1, "str1")
+
+var0[(var1 * 0) + 1]
+
+var0 = var1[var2.name]
+
+var0 = self.class.new
+
+acts_as_list_class.transaction do
+  increment_positions_on_higher_items
+  assume_top_position
+end
+
+options[:sym0].to_s
+
+raise(ArgumentError.new("str0#{var0}"))
+
+var0.unshift(File.expand_path(File.dirname("str0") + "str1"))
+
+[var0[var1], var0[var2]]
+
+(var0 & 0) == 0
+
+[var0[0], var0[1]]
+
+:sym0 => params[:sym1]
+
+var0 >= var1.length
+
+!block_given?
+
+warn("str0#{var0.inspect}str1")
+
+var0[var1].delete(var2)
+
+var0.split("str0").map do |var1|
+  var1.to_i
+end
+
+var0.errors.empty?
+
+((var0.backtrace) || ([])).join("str0")
+
+((var0["str0"]) && (var0["str1"]))
+
+(var0 << "str0") << var1.to_s
+
+!empty?
+
+var0.length != var1
+
+((var0.is_a?(Hash)) || (var0.is_a?(Array)))
+
+!var0.success?
+
+var0.result(binding)
+
+!var0.key?(var1)
+
+def method
+  template("str0", File.join("str1", class_path, "#{file_name}str2"))
+end
+
+File.basename(File.dirname(var0))
+
+update_attribute(position_column, self.send(position_column).to_i - 0)
+
+var0 << "str0#{name}str1"
+
+((var0 == nil) || (var1 == nil))
+
+var0.class.new
+
+var0[0] =~ /str0/
+
+var0[:sym0].kind_of?(Array)
+
+self.class.logger
+
+var0[var1].last
+
+var0.name.blank?
+
+raise(NotImplementedError.new)
+
+raise(NotImplementedError.new("str0"))
+
+var0 = var1[:sym0]
+var2 = var1[:sym1]
+
+var0["str0"] != "str1"
+
+"str0" + $1.unpack("str1" * $1.size).join("str0").upcase
+
+require("str0")
+ENV["str1"] ||= File.expand_path("str2", "str3")
+if File.exists?(ENV["str1"])
+  require("str4")
+end
+
+var0.identifier.to_s
+
+"#{var0[0]}str0#{var0[1]}"
+
+var0 = var1[var2.downcase]
+
+var0.to_i != 0
+
+puts(var0)
+exit
+
+var0 = open(var1, (File::WRONLY | File::APPEND) | File::CREAT)
+
+var0.value.nil?
+
+var0.sub(/str0#{Regexp.quote(var1)}str1/, "str2")
+
+var0.split("str0")[0..1].join("str0")
+
+var0 - var1.to_i
+
+var0 <= var1.size
+
+var0.length <= var1
+
+var0.attributes.empty?
+
+def method
+  if ((var0) && (!var0.closed?))
+    var0.close
+  end
+end
+
+var0.name[0, 1]
+
+Time.now.to_i.to_s
+
+((var0) && (var1[:sym0] != false))
+
+var0[0..1].to_sym
+
+var0 << sprintf("str0", var1)
+
+var0.each_byte do |var1|
+  var2 << sprintf("str0", var1)
+end
+
+((var0["str0"]) || (var0[:sym0]))
+
+((((_parse_eu(var0, var1)) || (_parse_us(var0, var1)))) || (_parse_iso(var0, var1)))
+
+((var0) && (var0[0, 1] == "str0"))
+
+var0.downcase == "str0"
+
+var0 << var1.matched
+
+var0.size.to_f
+
+/str0/.match(var0)[0]
+
+((var0 != :sym0) && (var0 != :sym1))
+
+config[:sym0]
+
+var0[:sym0].nil?
+
+((_parse_eu(var0, var1)) || (_parse_us(var0, var1)))
+
+var0 ||= Hash.new do |var1, var2|
+  var1[var2] = []
+end
+
+if File.directory?(var0)
+  var1.unshift(var0)
+end
+
+Time.now.strftime(var0)
+
+s3e(var0, $4, Format::ABBR_MONTHS[$1.downcase], $2, (($3) && ($3[0, 1].downcase == "str0")))
+
+var0, var1 = var2 >> 0, var2 & 1
+
+"str0" * var0 - var1
+
+def method(var0)
+  var1.find do |var2|
+    var2.name == var0
+  end
+end
+
+!var0.match(/str0/)
+
+var0 = var1[var2].ord
+
+"str0".%(var0[:sym0])
+
+var0.include?(var1.to_s)
+
+config.filter_parameters
+
+var0.flatten.map do |var1|
+  var1.to_s
+end
+
+var0.html do
+  render({:sym0 => "str0"})
+end
+
+session[var0]
+
+var0.puts("str0#{var1}")
+exit(0)
+
+var0 = var1.size / 0
+
+require("str0")
+require("str1")
+require("str2")
+require("str3")
+require("str4")
+
+id.to_s
+
+Net::HTTP.new(var0.host, var0.port)
+
+$3[0, 1].downcase == "str0"
+
+request.env
+
+var0.values_at(*var1).compact
+
+File.chmod(0, var0.path)
+
+var0.camelize.constantize
+
+[var0[:sym0], var0[:sym1], var0[:sym2], var0[:sym3]]
+
+if var0.has_key?(var1)
+  return(var0[var1])
+end
+
+options[:sym0] == true
+
+var0 = Time.now - var1
+
+var0.keys.sort.last
+
+var0.transaction do |var1|
+  File.chmod(0, var1.path)
+end
+
+var0.set_backtrace(var1 + caller)
+
+var0["str0"].length
+
+(var0 * 0) + var1
+
+"str0#{options[:sym0]}"
+
+self.class.new(+d)
+
+attributes[var0]
+
+var0.value == value
+
+$2.to_i * 0
+
+var0.length - var1.length
+
+if var0
+  var1 = -var1 + 0
+end
+
+Marshal.load(Marshal.dump(var0))
+
+File.expand_path(File.dirname("str0"))
+
+var0 == var1 - 0
+
+!var0[var1].nil?
+
+(var0 + var1) + 0
+
+raise(ArgumentError, "str0#{var0.length}str1")
+
+var0[0, var1 - 1]
+
+var0 = var0[0..var1 - 1]
+
+"#{position_column}str0#{position_column}str1"
+
+var0.each do |var1, var2|
+  instance_variable_set("str0#{var1}", var2)
+end
+
+var0 + var1.size
+
+var0.length < var1.length
+
+update_attribute(position_column, 0)
+
+var0 << var1[0]
+
+unless ((var0) && (!var0.empty?))
+  next
+end
+
+var0 << var1.join("str0")
+
+acts_as_list_class.update_all("#{position_column}str0#{position_column}str1", "#{scope_condition}str2#{position_column}str3#{var0}")
+
+var0 << var1["str0"]
+
+var0.each do |var1, var2|
+  var3[var1] = var2
+end
+
+var0.children.each
+
+var0.bytesize.to_s
+
+unless var0.empty?
+  (var1 << "str0") << var0
+end
+
+var0["str0"]["str1"]
+
+((var0 > 0) && (var1 > 0))
+
+"str0" + var0.name
+
+var0.unpack("str0").first
+
+var0 = self.class.name
+
+var0.first.size
+
+var0.to_s =~ /str0/i
+
+puts("str0" * 0)
+
+if var0
+  var1[:sym0] = var0.to_i
+end
+
+var0 = File.expand_path(File.dirname("str0"))
+
+var0.force_encoding(Encoding.default_external)
+
+var0[0, 1] + "str0"
+
+var0.to_a.sort
+
+session[:sym0]
+
+self.class === var0
+
+var0[0].to_s
+
+raise("str0#{var0.inspect}str1")
+
+var0.cookies[var1]
+
+!var0.kind_of?(Array)
+
+var0.uniq.join("str0")
+
+var0[0] - 1
+
+var0[:sym0].is_a?(Symbol)
+
+def method
+  if File.exist?(preinitializer_path)
+    load(preinitializer_path)
+  end
+end
+
+var0.push(var1[0])
+
+config.active_record
+
+var0.backtrace.join("str0")
+
+var0[var1].ord
+
+var0.keys.sort.map
+
+(var0 + var1) - 0
+
+define_method("#{var0}str0") do
+  var1 == var0
+end
+
+:sym0 => options[:sym1]
+
+var0[var1].each
+
+var0.join("str0") + "str0"
+
+if ((var0 == "str0") || (var0 == "str1"))
+  next
+end
+
+var0[0..0] == "str0"
+
+open(var0, "str0") do |var1|
+  var1.write(var2)
+end
+
+var0.dup.force_encoding("str0")
+
+(var0 - var1.length) + 0
+
+if File.exists?(ENV["str0"])
+  require("str1")
+end
+
+var0 = (var1 >> 0) & 1
+
+var0.collect do |var1|
+  var1.dup
+end
+
+0..var0.size - 1
+
+self.class.name
+
+var0.path.to_s
+
+parameters[:sym0]
+
+var0.to_f / var1.to_f
+
+var0.each do |var1|
+  self << var1
+end
+
+((var0 >= 0) && (var0 <= 1))
+
+var0 = var1.split("str0").last
+
+var0.size.times
+
+config.consider_all_requests_local=(false)
+
+Post.new(var0.path)
+
+var0.map(&:sym0).join("str0")
+
+size > 0
+
+def method
+  self.class.new
+end
+
+var0.name == :sym0
+
+(var0.size - 0).times
+
+var0.split(/str0/).map
+
+var0.class == Hash
+
+(var0 - var1).abs
+
+!var0.singleton
+
+var0.params["str0"]
+
+if var0.last.is_a?(Hash)
+  var1 = var0.pop
+end
+
+var0 = var1.length - 0
+
+var0 = Time.now + var1
+
+var0[var1[0]] = var2
+
+var0 * var1.to_i
+
+"#{scope_condition}str0#{position_column}str1#{send(position_column).to_i}"
+
+var0.keys.first
+
+var0.message =~ /str0/
+
+var0 = var1.result(binding)
+
+Rails.root.join("str0", "str1")
+
+var0.size >= 0
+
+var0.to_f * 0
+
+var0.exitstatus != 0
+
+load(preinitializer_path)
+
+var0.sub!(/str0/, "str1").sub!(/str2/, "str1")
+
+if var0[:sym0]
+  var1[:sym0] = var0[:sym0]
+end
+
+params[:sym0].present?
+
+var0.text =~ /str0/
+
+(["str0"] * 0).join("str1")
+
+(var0 - var1) + 0
+
+File.join(RbConfig::CONFIG["str0"], RbConfig::CONFIG["str1"] + RbConfig::CONFIG["str2"])
+
+File.join(File.dirname("str0"), "str1")
+
+((var0.size == 0) && (var1.size == 0))
+
