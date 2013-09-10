558ec6348ab38e77a3b40d5df05ba9a9bc5edf7e
abhishek <bigbeliever@gmail.com>
2013-09-10 14:53:27 +0530

    99 - Non-zero size

--- patterns.rb
+++ patterns.rb
@@ -1562,6 +1562,18 @@ end
 
 var0 << var1.slice!(0, var2)
 
+# 99.
+#
+# Title: Non-zero size
+# Category: [ Standard lib ]
+#
+# Description:
+# Checks if the size of var0 is greater than 0
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 var0.size > 0
 
 var0.type == "str0"
