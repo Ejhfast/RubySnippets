c0dd7d5a0b5f151cddd5af4194b8b7491e1e6602
abhishek <bigbeliever@gmail.com>
2013-09-11 15:31:48 +0530

    116 - Value in nested hash

--- patterns.rb
+++ patterns.rb
@@ -1829,6 +1829,18 @@ caller.join("str0")
 
 var0 + caller
 
+# 116.
+#
+# Title: Value in nested hash
+# Category: [ Standard lib ]
+#
+# Description:
+# Gets the value at 'str0' from a hash nested at :sym0 within the hash var0
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 var0[:sym0]["str0"]
 
 var0.data["str0"]
