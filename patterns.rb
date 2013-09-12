e3f0b03968db733d4f5ba10fc9504d26161a033d
abhishek <bigbeliever@gmail.com>
2013-09-13 04:12:04 +0530

    151 - Element of an array as string

--- patterns.rb
+++ patterns.rb
@@ -2387,6 +2387,18 @@ config.action_controller.consider_all_requests_local=(true)
 
 Thread.current[:sym0] = []
 
+# 151.
+#
+# Title: Element of an array as string
+# Category: [ Standard lib ]
+#
+# Description:
+# Stringify an element (first here) of array var1 and assign it to var0
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 var0 = var1[0].to_s
 
 var0[var1] = var2.clone
