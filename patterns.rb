d830b03edd3977385266f28f45f8aa7cf7fb6c2d
abhishek <bigbeliever@gmail.com>
2013-09-18 17:43:48 +0530

    297 - Check presence of hash elements

--- patterns.rb
+++ patterns.rb
@@ -4674,6 +4674,18 @@ var0.errors.empty?
 
 ((var0.backtrace) || ([])).join("str0")
 
+# 297.
+#
+# Title: Check presence of hash elements
+# Category: [ Standard lib ]
+#
+# Description:
+# Checks if the hash 'var0' contains elements for keys 'str0' and 'str1'
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 ((var0["str0"]) && (var0["str1"]))
 
 (var0 << "str0") << var1.to_s
