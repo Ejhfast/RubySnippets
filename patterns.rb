46b8afe1dc8f49675f1b93e74db47c4d0813c839
abhishek <bigbeliever@gmail.com>
2013-09-19 01:16:10 +0530

    304 - Hash content check

--- patterns.rb
+++ patterns.rb
@@ -4777,6 +4777,18 @@ var0.length != var1
 
 var0.result(binding)
 
+# 304.
+#
+# Title: Hash content check
+# Category: [ Standard lib ]
+#
+# Description:
+# Checks if the hash 'var0' (doesn't) contain the key 'var1'
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 !var0.key?(var1)
 
 def method
