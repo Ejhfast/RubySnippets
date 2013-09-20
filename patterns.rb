461d5b2fa2f2a01946beda207077a9c9c04e48eb
abhishek <bigbeliever@gmail.com>
2013-09-21 03:47:17 +0530

    354 - Add directory to array

--- patterns.rb
+++ patterns.rb
@@ -5550,6 +5550,19 @@ var0 ||= Hash.new do |var1, var2|
   var1[var2] = []
 end
 
+# 354.
+#
+# Title: Add directory to array
+# Category: [ Standard lib ]
+#
+# Description:
+# If 'var0' is a directory, or a symlink to a directory, then add it to the beginning
+# of the array 'var1'.
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 if File.directory?(var0)
   var1.unshift(var0)
 end
