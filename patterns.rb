ea3f73c0eab860f549f0abce916c89128868ac40
abhishek <bigbeliever@gmail.com>
2013-09-23 03:03:23 +0530

    464 - Last part of split variable

--- patterns.rb
+++ patterns.rb
@@ -7277,6 +7277,18 @@ end
 
 ((var0 >= 0) && (var0 <= 1))
 
+# 464.
+#
+# Title: Last part of split variable
+# Category: [ Standard lib ]
+#
+# Description:
+# Splits the 'var1' at 'str0' and assign the last part to 'var0'.
+#
+# Useful programming task or idiom: [ Yes ]
+# Can be encapsulated in standalone function: [ Yes ]
+# There's a more common way to write the code snippet: [ No ]
+
 var0 = var1.split("str0").last
 
 var0.size.times
